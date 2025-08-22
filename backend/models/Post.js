ed08c9b4d05c30539fd57fd0c6e6e12553570923
muhammadrafayasif <muhammadrafay3561@gmail.com>
2025-08-22 11:15:32 +0500

    Fixed some issues for deployment

--- backend/models/Post.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const post = new mongoose.Schema({
-  content: { type: String, required: true },
-  title: { type: String, required: true },
-  posted_by: { type: String, required: true },
-}, {timestamps: true})
-
-export default mongoose.model("Post", post);
